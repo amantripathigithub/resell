7f81026e07642d6a7df03b0ae7b2b092e75fcfc3
amantripathigithub <at425020@gmail.com>
2023-11-03 20:31:02 +0530

    login signup pages

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 
 const port = 3000; // You can change the port to any available port you prefer
+app.set('view engine', 'ejs');
+const path = require("path");
 
+//app.use(express.static('..//frontend'));
 // Define a route that responds with "Hello, World!" for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    app.use(express.static("../frontend"));
+    res.render(path.join(__dirname, "../frontend", "/signup.ejs"));
 });
 
 app.listen(port, () => {
