26f6127fca0ee45a39cd0ccaa74f7a4625819695
amantripathigithub <at425020@gmail.com>
2023-11-03 21:50:06 +0530

    login signup pages

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
 const port = 3000; // You can change the port to any available port you prefer
 app.set('view engine', 'ejs');
 const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static('..//frontend'));
 // Define a route that responds with "Hello, World!" for the root path
+
+const mongoose = require('mongoose');
+
+
+
+dotenv.config({ path: './file.env' });
+const DB = process.env.DATABASE;
+
+
+console.log(DB);
+
+mongoose.connect(DB).then(()=>{
+    console.log("connected to database");
+}).catch((err)=>{
+    console.log("not connected to database");
+});
+
+
 app.get('/', (req, res) => {
     app.use(express.static("../frontend"));
     res.render(path.join(__dirname, "../frontend", "/home.ejs"));
@@ -27,6 +45,10 @@ app.post("/signup",(req,res)=>{
     const username=req.body.username;
     const password=req.body.password;
     console.log(email+username+password);
+
+
+
+
 })
 
 
